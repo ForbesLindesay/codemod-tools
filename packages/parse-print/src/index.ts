b6b1438bc0ffff21fcba57d554b28f78862c4c8f
Forbes Lindesay <forbes@lindesay.co.uk>
2020-08-19 23:16:27 +0100

    feat: add template helpers (#3)

--- packages/parse-print/src/index.ts
+++ packages/parse-print/src/index.ts
@@ -1,5 +1,9 @@
 import {parse as babelParse, ParserOptions} from '@babel/parser';
 import * as t from '@babel/types';
+import template, {
+  TemplateBuilder,
+  TemplateBuilderOptions,
+} from '@babel/template';
 import analyzeScope, {
   ScopeInfo,
   Scope,
@@ -58,6 +62,17 @@ export default function parse(
   return {
     ...ctx,
     root: pathCTX.path(ast, () => []),
+    template: {
+      statement: syntacticTemplate(
+        template.statement({...opts, ranges: false}),
+      ),
+      statements: syntacticTemplate(
+        template.statements({...opts, ranges: false}),
+      ),
+      expression: syntacticTemplate(
+        template.expression({...opts, ranges: false}),
+      ),
+    },
     print(options) {
       const g = new Generator({
         source: src,
@@ -70,9 +85,40 @@ export default function parse(
   };
 }
 
+function syntacticTemplate<T>(builder: TemplateBuilder<T>) {
+  const builder2 = builder({
+    syntacticPlaceholders: true,
+  } as TemplateBuilderOptions);
+  const templateFnCache = new WeakMap<
+    TemplateStringsArray,
+    (arg: {[index: string]: unknown}) => T
+  >();
+  return (tpl: TemplateStringsArray, ...args: unknown[]) => {
+    let fn = templateFnCache.get(tpl);
+    if (!fn) {
+      fn = builder2(
+        tpl
+          .map((str, i) => (i === 0 ? str : `%%placeholder${i - 1}%%${str}`))
+          .join(''),
+      );
+      templateFnCache.set(tpl, fn);
+    }
+    const argsObj: {[index: string]: unknown} = {};
+    for (let i = 0; i < args.length; i++) {
+      argsObj[`placeholder${i}`] = args[i];
+    }
+    return fn(argsObj);
+  };
+}
+
 export interface ParsePrint {
   root: Path<t.File>;
   scope: ScopeInfo;
+  template: {
+    statement(tpl: TemplateStringsArray, ...args: unknown[]): t.Statement;
+    statements(tpl: TemplateStringsArray, ...args: unknown[]): t.Statement[];
+    expression(tpl: TemplateStringsArray, ...args: unknown[]): t.Expression;
+  };
   replace(node: t.Node, ...replacements: t.Node[]): void;
   insertBefore(node: t.Node, ...prefixes: t.Node[]): void;
   insertAfter(node: t.Node, ...suffixes: t.Node[]): void;
