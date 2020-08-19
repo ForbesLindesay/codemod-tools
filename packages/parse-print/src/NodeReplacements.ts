d7149dfc83f93c59969daea3229c1879ddf3ef7c
Forbes Lindesay <forbes@lindesay.co.uk>
2020-08-19 22:32:42 +0100

    feat: add methods for insertBefore and insertAfter (#2)

--- packages/parse-print/src/NodeReplacements.ts
+++ packages/parse-print/src/NodeReplacements.ts
@@ -1,11 +1,34 @@
 import * as t from '@babel/types';
 
 export default class NodeReplacements {
+  private readonly _prefixes = new Map<t.Node, t.Node[]>();
   private readonly _replacements = new Map<t.Node, t.Node[]>();
+  private readonly _suffixes = new Map<t.Node, t.Node[]>();
   public resolve(node: t.Node) {
-    return this._replacements.get(node);
+    const prefix = this._prefixes.get(node);
+    const replacement = this._replacements.get(node);
+    const suffix = this._suffixes.get(node);
+    if (prefix || replacement || suffix) {
+      return [...(prefix || []), ...(replacement || [node]), ...(suffix || [])];
+    }
+    return undefined;
   }
+
   public replace(node: t.Node, ...replacements: t.Node[]) {
     this._replacements.set(node, replacements);
+  }
+
+  public insertBefore(node: t.Node, ...prefixes: t.Node[]) {
+    this._prefixes.set(node, [
+      ...prefixes,
+      ...(this._prefixes.get(node) || []),
+    ]);
+  }
+
+  public insertAfter(node: t.Node, ...suffixes: t.Node[]) {
+    this._suffixes.set(node, [
+      ...(this._suffixes.get(node) || []),
+      ...suffixes,
+    ]);
   }
 }
--- packages/parse-print/src/index.ts
+++ packages/parse-print/src/index.ts
@@ -40,11 +40,24 @@ export default function parse(
   const ctx = {
     scope: analyzeScope(ast),
     replace: replacements.replace.bind(replacements),
+    insertBefore: replacements.insertBefore.bind(replacements),
+    insertAfter: replacements.insertAfter.bind(replacements),
     overridePrintOptions: overrides.setOverride.bind(overrides),
+  };
+  const pathCache = new Map<t.Node, Path<any>>();
+  const pathCTX = {
+    ...ctx,
+    path<T extends t.Node>(node: T, parents: () => t.Node[]): Path<T> {
+      const cached = pathCache.get(node);
+      if (cached) return cached;
+      const created = new Path(node, parents(), pathCTX);
+      pathCache.set(node, created);
+      return created;
+    },
   };
   return {
     ...ctx,
-    root: new Path(ast, [], ctx),
+    root: pathCTX.path(ast, () => []),
     print(options) {
       const g = new Generator({
         source: src,
@@ -61,13 +74,18 @@ export interface ParsePrint {
   root: Path<t.File>;
   scope: ScopeInfo;
   replace(node: t.Node, ...replacements: t.Node[]): void;
+  insertBefore(node: t.Node, ...prefixes: t.Node[]): void;
+  insertAfter(node: t.Node, ...suffixes: t.Node[]): void;
   overridePrintOptions(node: t.Node, options: PrintOptions): void;
   print(options?: PrintOptions): string;
 }
 
 interface PathContext {
+  path<T extends t.Node>(node: T, parents: () => t.Node[]): Path<T>;
   scope: ScopeInfo;
   replace(node: t.Node, ...replacements: t.Node[]): void;
+  insertBefore(node: t.Node, ...prefixes: t.Node[]): void;
+  insertAfter(node: t.Node, ...suffixes: t.Node[]): void;
   overridePrintOptions(node: t.Node, options: PrintOptions): void;
 }
 
@@ -86,41 +104,78 @@ export class Path<T extends t.Node> {
   public replace(...replacements: t.Node[]) {
     this._ctx.replace(this.node, ...replacements);
   }
+  public insertBefore(...prefixes: t.Node[]) {
+    this._ctx.insertBefore(this.node, ...prefixes);
+  }
+  public insertAfter(...suffixes: t.Node[]) {
+    this._ctx.insertAfter(this.node, ...suffixes);
+  }
+
   public overridePrintOptions(options: PrintOptions) {
     this._ctx.overridePrintOptions(this.node, options);
   }
 
   // Navigation Methods
 
+  public get parentPath() {
+    return this._ctx.path(this.parents[0], () => this.parents.slice(1));
+  }
+
+  public get<TKey extends keyof T>(
+    key: TKey,
+  ): T[TKey] extends t.Node
+    ? Path<T[TKey]>
+    : T[TKey] extends (infer E)[]
+    ? (E extends t.Node ? Path<E> : E)[]
+    : T[TKey] {
+    let parentsCache: t.Node[] | undefined;
+    const parents = () =>
+      parentsCache || (parentsCache = [this.node, ...this.parents]);
+    const value: any = this.node[key];
+    if (t.isNode(value)) {
+      return this._ctx.path(value, parents) as any;
+    }
+    if (Array.isArray(value)) {
+      return value.map((v) => {
+        if (t.isNode(v)) {
+          return this._ctx.path(v, parents);
+        }
+        return v;
+      }) as any;
+    }
+    return value;
+  }
+
   public find<T extends t.Node>(filter: NodeFilter<T>) {
-    return filter.find(
-      this.node,
-      (node, parents) =>
-        new Path(
-          node,
-          [...parents.slice().reverse().slice(1), ...this.parents],
-          this._ctx,
-        ),
+    return filter.find(this.node, (node, parents) =>
+      this._ctx.path(node, () => [
+        ...parents.slice().reverse().slice(1),
+        ...this.parents,
+      ]),
     );
   }
 
-  private _toPath<T extends t.Node>({
-    node,
-    parents,
-  }: {
-    node: T;
-    parents: t.Node[];
-  }) {
-    return new Path(node, parents, this._ctx);
+  public findClosestParent<T extends t.Node>(filter: NodeFilter<T>) {
+    const i = this.parents.findIndex(filter);
+    if (i === -1) {
+      return undefined;
+    }
+    return this._ctx.path(this.parents[i] as T, () =>
+      this.parents.slice(i + 1),
+    );
   }
 
   public findDeclaration(this: Path<t.Identifier>) {
     const declaration = this._ctx.scope.declarations.get(this.node);
-    return declaration && this._toPath(declaration);
+    return (
+      declaration && this._ctx.path(declaration.node, () => declaration.parents)
+    );
   }
 
   public findReferences(this: Path<t.Identifier>) {
     const references = this._ctx.scope.references.get(this.node);
-    return (references || []).map((r) => this._toPath(r));
+    return (references || []).map((r) =>
+      this._ctx.path(r.node, () => r.parents),
+    );
   }
 }
